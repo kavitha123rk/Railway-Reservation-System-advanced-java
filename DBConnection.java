de636f170426c636c34d7c5cdc1619f719ba0ab0
kavitha123rk <kavithak07012006@gmail.com>
2026-04-29 15:30:00 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -1,1 +1,16 @@
+import java.sql.*;
 
+public class DBConnection {
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/railway_system",
+                "root",
+                "root123"
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
